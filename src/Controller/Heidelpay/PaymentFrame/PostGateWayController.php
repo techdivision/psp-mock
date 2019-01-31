2681653a82d80d0bee8c7c7092c220f58ee6c18e
Lukas Kiederle <l.kiederle@techdivision.com>
2019-01-31 15:48:01 +0100

    updated heidelpaytemplates

--- src/Controller/Heidelpay/PaymentFrame/PostGateWayController.php
+++ src/Controller/Heidelpay/PaymentFrame/PostGateWayController.php
@@ -48,17 +48,20 @@ class PostGateWayController extends AbstractController
         LoggerInterface $logger,
         ObjectManager $objectManager,
         RequestToOrderMapper $requestToOrderMapper
-    ) {
+    )
+    {
         $this->logger = $logger;
         $this->objectManager = $objectManager;
         $this->requestToOrderMapper = $requestToOrderMapper;
 
         $this->response = new Response();
-        $this->response->headers->set('Content-Type', 'text/plain');
+        $this->response->headers->set('Content-Type', 'application/json;charset=UTF-8');
+        $this->response->headers->set('Transfer-Encoding', 'chunked');
+        $this->response->headers->set('Connection', 'close');
         $this->response->headers->set('Keep-Alive', 'timeout=2, max=1000');
         $this->response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         $this->response->headers->set('X-Content-Type-Options', 'nosniff');
-        $this->response->headers->set('X-XSS-Protection', '1; mode=block');
+        $this->response->headers->set('X-XSS-Protection', '1');
     }
 
     /**
@@ -68,9 +71,50 @@ class PostGateWayController extends AbstractController
     public function execute(Request $request)
     {
         $requestType = $request->get('request');
-        $responseData = [
-            'test' => 'APPROVED',
-        ];
+        $responseData = "{
+  \"NAME.FAMILY\": \"Kiederle\",
+  \"CRITERION.SDK_NAME\": \"Heidelpay\\\\PhpPaymentApi\",
+  \"IDENTIFICATION.TRANSACTIONID\": \"596\",
+  \"ADDRESS.COUNTRY\": \"AT\",
+  \"ADDRESS.STREET\": \"Teststraße 1\",
+  \"PRESENTATION.AMOUNT\": \"27.00\",
+  \"CRITERION.SDK_VERSION\": \"v1.4.1\",
+  \"ACCOUNT.EXPIRY_MONTH\": \"06\",
+  \"CRITERION.PUSH_URL\": \"https://project-community-edition.test/hgw/index/push/\",
+  \"PROCESSING.TIMESTAMP\": \"2019-01-25 08:25:04\",
+  \"CONTACT.EMAIL\": \"l.kiederle@techdivision.com\",
+  \"CRITERION.SHOP.TYPE\": \"Magento 2.2.7-Enterprise\",
+  \"ACCOUNT.BRAND\": \"VISA\",
+  \"PROCESSING.STATUS.CODE\": \"90\",
+  \"NAME.GIVEN\": \"Lukas\",
+  \"IDENTIFICATION.SHORTID\": \"4123.3110.4434\",
+  \"ADDRESS.CITY\": \"Testhausen\",
+  \"CLEARING.AMOUNT\": \"27.00\",
+  \"ACCOUNT.HOLDER\": \"Lukas Kiederle\",
+  \"PROCESSING.CODE\": \"CC.PA.90.00\",
+  \"PROCESSING.STATUS\": \"NEW\",
+  \"PROCESSING.RETURN.CODE\": \"000.100.112\",
+  \"CRITERION.PAYMENT_METHOD\": \"CreditCardPaymentMethod\",
+  \"PROCESSING.RESULT\": \"ACK\",
+  \"CRITERION.SHOPMODULE.VERSION\": \"Heidelpay Gateway 18.6.11\",
+  \"CLEARING.CURRENCY\": \"EUR\",
+  \"IDENTIFICATION.UNIQUEID\": \"31HA07BC8107D4D1EBBE090A63F698CF\",
+  \"CRITERION.SECRET\": \"86eafc5b7de669edd09bff29e9072c27e994c6814a59a7dc50fa6664117a34c185509bb577b42d740267880241bc261c3be90ac8b30c18142cf0de49ee22f3d2\",
+  \"ACCOUNT.EXPIRY_YEAR\": \"2024\",
+  \"PRESENTATION.CURRENCY\": \"EUR\",
+  \"PROCESSING.REASON.CODE\": \"00\",
+  \"ACCOUNT.VERIFICATION\": \"***\",
+  \"ADDRESS.ZIP\": \"1234\",
+  \"ACCOUNT.NUMBER\": \"471110******0000\",
+  \"CLEARING.DESCRIPTOR\": \"HSA*Kleen-Text|Kufstein|AT\",
+  \"FRONTEND.PREVENT_ASYNC_REDIRECT\": \"FALSE\",
+  \"PROCESSING.REASON\": \"SUCCESSFULL\",
+  \"CRITERION.GUEST\": \"true\",
+  \"PROCESSING.RETURN\": \"Request successfully processed in 'Merchant in Connector Test Mode'\",
+  \"PROCESSING.REDIRECT_URL\": \"https://project-community-edition.test/hgw/index/redirect/\",
+  \"FRONTEND.LANGUAGE\": \"EN\",
+  \"PAYMENT.CODE\": \"CC.PA\"
+}";
         return $this->buildResponse($responseData);
     }
 
@@ -106,17 +150,12 @@ class PostGateWayController extends AbstractController
     }
 
     /**
-     * @param array $data
+     * @param $data
      * @return Response
      */
-    private function buildResponse(array $data)
+    private function buildResponse($data)
     {
-        $body = [];
-        foreach ($data as $key => $value) {
-            $body[] = $key . '=' . $value;
-        }
-
-        $this->response->setContent(implode(PHP_EOL, $body));
+        $this->response->setContent($data);
         return $this->response;
     }
 }
